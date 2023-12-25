c4028ba81929ad7560b8b1898898c12a49bde934
Vinit <vinupatil43210@gmail.com>
2023-12-25 19:03:47 +0530

    starting

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactTyped from "react-typed";
-// import Button from "../../components/Button"
+import Button from "../components/Buttons"
 
 import { useEffect, useRef, useState } from 'react'
 
@@ -18,15 +18,19 @@ const Homepage = () => {
                 </div>
                 <div className='ml-10 text-left mt-8 w-1/2 text-xl font-medium tracking-wider text-slate-300'>As a budding talent in the realms of frontend and backend development, I blend creativity with technical prowess to sculpt digital experiences. Eager to infuse innovation into code, I bring fresh perspectives and a hunger to learn, making me a dynamic force poised to shape the future of web development.
                 </div>
-                {/* <div>
+                <div className='mt-6 ml-10'>
                     <Button
-                        label={"hlo"}
+                        variant="default"
+                        label={"Download CV"}
+                        size="lg"
+                        color='yellow'
                     />
-                </div> */}
+                    <a href='vin.jpg'></a>
+                </div>
                 {/* ABOUT ME */}
                 <div>
                     <div className='ml-10 mt-6 text-3xl font-medium tracking-wider text-green-700'>ABOUT ME</div>
-                    <div className='ml-10 w-1/2 ml-10 mt-6 text-2xl font-medium tracking-wider text-green-700'>Passionate Computer Engineering student specializing in frontend and backend development with expertise in React.js and Node.js</div>
+                    <div className='ml-10 w-1/2 ml-10 mt-6 text-2xl font-medium tracking-wider text-green-700'>Code artisan sculpting seamless experiences with React.js and Node.js magic.</div>
                     <div className='text-xl ml-10 mt-6 text-green-100 w-1/2'>Hello! I am a third-year student pursuing Computer Engineering at R. C. Patel Institute of Technology. My journey in the field of technology has been dynamic and exciting. I specialize in both frontend and backend development, with a focus on creating seamless and responsive applications using React.js for the frontend and Node.js for the backend.
                     </div>
                 </div>
